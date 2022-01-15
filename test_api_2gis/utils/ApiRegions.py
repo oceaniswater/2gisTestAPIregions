88593259a2f9eae9a759cbb25b64373c383e02c7
markgolubev <golubev@ucheba.ru>
2022-01-15 17:33:47 +0300

    Подключил логгер, теперь при любом запросе будет проверятся статус-код. Если он будет >= 400 и < 500, то в лог будет выводиться статус-код и айди ошибки. Если статус код == 500 статус код просто будет выводиться в лог.

--- test_api_2gis/utils/ApiRegions.py
+++ test_api_2gis/utils/ApiRegions.py
@@ -1,11 +1,17 @@
 import configparser
+from concurrent.futures._base import LOGGER
+
 import requests
 import os
+import logging
+
+LOGGER = logging.getLogger(__name__)
 
 
 class ApiRegions:
     parser = configparser.ConfigParser()
     parser.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'config.ini'))
+
 
     BASE_URL = parser.get('2gis', 'basic_url')
 
@@ -17,5 +23,12 @@ class ApiRegions:
         headers = {'Content-Type': 'application/json'}
         payload = args
         result = requests.request("GET", url, headers=headers, params=payload)
+
+        if result.status_code >= 400:
+            if result.status_code < 500:
+                response_json = result.json()
+                LOGGER.info(f"Error code: {result.status_code} -- id: {response_json['error']['id']}")
+        if result.status_code == 500:
+            LOGGER.info(f"Error code: {result.status_code}")
 
         return result
